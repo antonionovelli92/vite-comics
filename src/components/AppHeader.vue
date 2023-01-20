989c834f216404b671142e603fa4a13860b9af33
antonionovelli92 <anto.novelli@hotmail.it>
2023-01-20 16:04:23 +0100

    add main

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,7 +3,18 @@ export default {
     name: 'AppHeader',
     data() {
         return {
-
+            links: [
+                { text: 'Characters', url: '#', current: false },
+                { text: 'Comics', url: '#', current: true },
+                { text: 'Movies', url: '#', current: false },
+                { text: 'TV', url: '#', current: false },
+                { text: 'Games', url: '#', current: false },
+                { text: 'Collectibles', url: '#', current: false },
+                { text: 'Videos', url: '#', current: false },
+                { text: 'Fans', url: '#', current: false },
+                { text: 'News', url: '#', current: false },
+                { text: 'Shop', url: '#', current: false }
+            ]
         }
     },
 
@@ -13,9 +24,53 @@ export default {
 
 <template>
 
+    <header>
+        <figure class="nav-logo">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </figure>
+        <ul class="nav-links">
+            <li v-for="link in links" :key="link.text">
+                <a :href="link.url" :class="{ 'active': link.current }">{{ link.text }}</a>
+            </li>
+        </ul>
+    </header>
+
+
 </template>
 
 
-<style>
+<style lang="scss">
+img {
+    max-width: 100%;
+    height: 75px;
+}
 
+header {
+    width: 100%;
+    height: 80px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 1.25rem 0;
+
+    .nav-links {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-end;
+        padding: 0;
+
+        li {
+            list-style: none;
+
+            a {
+                display: inline-block;
+                text-decoration: none;
+                color: #524647;
+                text-transform: uppercase;
+                margin: 0 0.75rem;
+
+            }
+        }
+    }
+}
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -12,10 +12,35 @@ export default {
 </script>
 
 <template>
+    <main>
+        <section class="section-black">
+            <h2> --> Content goese here! </h2>
+        </section>
+        <section class="section-blu"></section>
+    </main>
 
 </template>
 
 
-<style>
+<style lang="scss">
+.section-black {
+    width: 100%;
+    height: 120px;
+    background-color: #1C1C1C;
 
+    h2 {
+        display: flex;
+        margin: 1.25rem 0;
+        color: white;
+        height: 100%;
+        align-items: center;
+    }
+}
+
+.section-blu {
+    width: 100%;
+    height: 140px;
+    background-color: #0382F9;
+
+}
 </style>
